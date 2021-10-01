6562506bb8299ee9c12feb97a2fe1499a76af874
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2021-10-01 11:03:50 +0200

    Create R files

---
+++ R/openRspat.R
@@ -0,0 +1,10 @@
+#' Open source geo-spatial analysis
+#'
+#' Contain functions to download free & open geo-datasets, convert projection of the sf objects, and geospatial analysis of the datasets.
+#'
+#' @docType package
+#'
+#' @author Om Prakash Bhandari \email{om.bhandari1995@gmail.com}
+#'
+#' @name openRspat
+NULL
