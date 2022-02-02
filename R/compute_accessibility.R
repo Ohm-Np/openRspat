041f49699c12af4ad168c53c1ed839c475fe4687
Ohm-Np <om.bhandari1995@gmail.com>
2022-02-02 13:58:04 +0000

    create new function compute_accessibility

--- R/compute_accessibility.R
+++ R/compute_accessibility.R
@@ -35,3 +35,61 @@ get_accessibility <- function(range, index, path) {
     }
   )
 }
+
+
+#' Compute zonal statistics for travel time from the area of interest to the nearby cities
+#'
+#' Returns the result in long-table format
+#'
+#' @param aoi area of interest - polygon (sf)
+#'
+#' @param acc_rast accessibility raster (terra)
+#'
+#' @param idcol A character vector identifying a column name which uniquely
+#' identifies the polygons in the \code{aoi} object.
+#'
+#' @param opn A type of zonal operation (e.g. sum, mean, min, max)
+#'
+#' @importFrom dplyr %>%
+#' @importFrom dplyr filter
+#' @importFrom tidyr pivot_longer
+#'
+#' @return zonal statistics
+#'
+#' @examples
+#'
+#' # load raster
+#' r <- terra::rast(system.file("extdata", "test_acc_rast.tif", package = "openRspat"))
+#' # load polygon
+#' fname <- system.file("shape/nc.shp", package="sf")
+#' sf <- sf::st_read(fname)
+#' # provide unique id column
+#' idcol <- "CNTY_ID"
+#' # zonal operation type
+#' opn <- "min"
+#' # call function
+#' compute_accessibility(sf[1,], r, idcol, opn)
+#'
+#' @export
+#'
+
+
+compute_accessibility <- function(aoi, acc_rast, idcol, opn) {
+
+  tryCatch(
+    {
+
+      zstats <-
+        openRspat::zonal_operation(aoi, acc_rast, idcol, opn)
+      travel_time_to_nearby_cities <- NULL
+      df <- data.frame(ID=idcol,
+                       travel_time_to_nearby_cities=zstats[, 2])
+      zstats_longer <- tidyr::pivot_longer(df,
+                                    cols = travel_time_to_nearby_cities)
+      return(zstats_longer)
+    },
+    error = function(e) {
+      message('Error in this line!')
+    }
+  )
+}
